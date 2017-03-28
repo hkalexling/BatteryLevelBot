1db141ecbf632a73925347fdb54ff74d376e5e77
Alex Ling <hkalexling@gmail.com>
2017-03-28 10:22:57 +0800

    -

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const tweet_interval_min = 5;
+const threshold_battery = 20;
+const threshold_aspect_ratio = 0.75;
+const track_keywork = 'i';
+
 var Twit = require('twit');
 var request = require('request');
 var fs = require('fs');
@@ -9,7 +14,27 @@ var ocr = require('./ocr');
 
 var T = new Twit(secret.twitter);
 
-var stream = T.stream('statuses/filter', {track: 'i'});
+var stream = T.stream('statuses/filter', {track: track_keywork});
+
+var reply = null; // {id: '12345678', msg: 'Hello World!'}
+setInterval(() => {
+	if (reply === null) return;
+
+	T.post('statuses/update', {
+		status: reply.msg,
+		in_reply_to_status_id: reply.id
+	}, (err, data, response) => {
+		reply = null;
+		if (err) {
+			console.log('failed to reply with error: ', err);
+		}
+		else{
+			console.log('replied!');
+		}
+		console.log('reply: ', reply);
+	});
+
+}, 1000 * 60 * tweet_interval_min); // minimum tweet interval: 15min
 
 stream.on('connect', req => {
 	console.log('stream connecting');
@@ -35,7 +60,7 @@ stream.on('tweet', tweet => {
 		media.forEach(m => {
 			if (m.type === 'photo'){
 				var smallSize = m.sizes.small;
-				if (smallSize.w / smallSize.h < 0.75){
+				if (smallSize.w / smallSize.h < threshold_aspect_ratio){
 					
 					tempFile(m.media_url, (path, del) => {
 						ocr.process(path, (err, percentage) => {
@@ -44,20 +69,12 @@ stream.on('tweet', tweet => {
 								console.log('err: ', err);
 							}
 							if (percentage) {
-								var num = parseInt(percentage.replace('%', ''));
-								if (num <= 20){
 
-									T.post('statuses/update', {
-										status: `@${tweet.user.screen_name} Your phone has only ${num}% of battery left! Go charge it! :D`,
-										in_reply_to_status_id: tweet.id_str
-									}, (err, data, response) => {
-										if (err) {
-											console.log('failed to reply with error: ', err);
-										}
-										else{
-											console.log('replied!');
-										}
-									});
+								var num = parseInt(percentage.replace('%', ''));
+								if (num <= threshold_battery){
+
+									reply = getReply(tweet.id_str, num, tweet.user.screen_name);
+									console.log('reply added: ', reply);
 
 								}
 							}
@@ -80,4 +97,14 @@ function tempFile(url, handler){
 	request(url).pipe(writeStream).on('close', () => {
 		handler(savePath, del);
 	});
+}
+
+function getReply(id, num, name){
+	var msgPool = [
+		`@${name} Your phone has only ${num}% of battery left! Go Charge it! 😉`,
+		`@${name} Hi there, remember to charge your phone! It has only ${num}% battery left! 🔋`,
+		`@${name} 😇 Just a friendly reminder: your phone has only ${num}% battery left and needs to be charged!`
+	];
+	var msg = msgPool[Math.floor(Math.random() * msgPool.length)];
+	return {id: id, msg: msg};
 }
